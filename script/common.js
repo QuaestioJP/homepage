ed7951076fc4f36ccd8836d22dae294c4c78fa20
ZIP <56752821+1234Yosuke@users.noreply.github.com>
2022-05-05 20:14:44 +0900

    [add] header
    ヘッダー作ったよ(?)

---
+++ script/common.js
@@ -0,0 +1,18 @@
+window.onload = () => {
+    document.body.innerHTML = `
+        <header>
+            <a id="head-logo"><img src="/img/head-logo.png" height="50vw"></a>
+            <div id="head-nav">
+                <ul id="head-nav-ul">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/project">Project</a></li>
+                    <li><a href="/blog">Blog</a></li>
+                </ul>
+            </div>
+        </header>
+        ${document.body.innerHTML}
+        <footer>
+           <p>&copy; 2022 Quaestio</p>
+        </footer>`
+}
